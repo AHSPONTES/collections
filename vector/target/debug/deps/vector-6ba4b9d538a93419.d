1b1cdcf01e5c478bcba6a10276bccb4893dd748b
AndreHSP <andrehspontes@gmail.com>
2021-07-19 08:12:39 -0300

    vector

---
+++ vector/target/debug/deps/vector-6ba4b9d538a93419.d
@@ -0,0 +1,5 @@
+/home/andre/Projetos/book_BeginningRustProgramming/collections/vector/target/debug/deps/vector-6ba4b9d538a93419.rmeta: src/main.rs
+
+/home/andre/Projetos/book_BeginningRustProgramming/collections/vector/target/debug/deps/vector-6ba4b9d538a93419.d: src/main.rs
+
+src/main.rs:
---
+++ vector/target/debug/deps/vector-fcc3870400d06f76.d
@@ -0,0 +1,5 @@
+/home/andre/Projetos/book_BeginningRustProgramming/collections/vector/target/debug/deps/vector-fcc3870400d06f76.rmeta: src/main.rs
+
+/home/andre/Projetos/book_BeginningRustProgramming/collections/vector/target/debug/deps/vector-fcc3870400d06f76.d: src/main.rs
+
+src/main.rs:
