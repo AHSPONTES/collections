01546a55e7a5dcb274bc90c33035a7cb39cf022a
AndreHSP <andrehspontes@gmail.com>
2021-07-19 09:36:32 -0300

    ok

---
+++ linked_list/target/debug/deps/tree-e95d1cb124d63676.d
@@ -0,0 +1,5 @@
+/home/andre/Projetos/book_BeginningRustProgramming/collections/tree/target/debug/deps/tree-e95d1cb124d63676: src/main.rs
+
+/home/andre/Projetos/book_BeginningRustProgramming/collections/tree/target/debug/deps/tree-e95d1cb124d63676.d: src/main.rs
+
+src/main.rs:
