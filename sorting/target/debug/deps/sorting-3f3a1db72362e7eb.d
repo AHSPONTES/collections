3e5abe215ce29a947017d76a841df771d80aea88
AndreHSP <andrehspontes@gmail.com>
2021-07-19 10:03:14 -0300

    ok

---
+++ sorting/target/debug/deps/sorting-3f3a1db72362e7eb.d
@@ -0,0 +1,5 @@
+/home/andre/Projetos/book_BeginningRustProgramming/collections/sorting/target/debug/deps/sorting-3f3a1db72362e7eb: src/main.rs
+
+/home/andre/Projetos/book_BeginningRustProgramming/collections/sorting/target/debug/deps/sorting-3f3a1db72362e7eb.d: src/main.rs
+
+src/main.rs:
---
+++ sorting/target/debug/sorting.d
@@ -0,0 +1,1 @@
+/home/andre/Projetos/book_BeginningRustProgramming/collections/sorting/target/debug/sorting: /home/andre/Projetos/book_BeginningRustProgramming/collections/sorting/src/main.rs
